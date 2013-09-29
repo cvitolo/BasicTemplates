73aff0c05f650640f9f125e45485920c5de86395
kadyna <claudia.vitolo@gmail.com>
2013-09-30 00:54:57 +0100

    Added Template.R

---
+++ Template.R
@@ -0,0 +1,67 @@
+#' This function calculates C
+#' 
+#' @author XXX
+#' 
+#' @description Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed 
+#' do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+#' minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+#' ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+#' velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+#' cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+#' est laborum.
+#' 
+#' @param A this is a number in the range [0,1]      
+#' @param B this is a number in the range [0,1] 
+#' 
+#' @return number in the range [0,1]
+#' 
+#' @examples 
+#' # C <- CalculateC(A,B)
+#' 
+#' Full list:
+#' @aliases
+#' @author
+#' @concepts
+#' @aliases
+#' @description
+#' @details
+#' @docType
+#' @example
+#' @examples
+#' @export
+#' @exportClass
+#' @exportMethod
+#' @family
+#' @format
+#' @import
+#' @importClassesFrom
+#' @importFrom
+#' @importMethodsFrom
+#' @include
+#' @inheritParams
+#' @keywords
+#' @method
+#' @name
+#' @note
+#' @param
+#' @rdname
+#' @references
+#' @return
+#' @S3method
+#' @section
+#' @seealso
+#' @source
+#' @template
+#' @templateVar
+#' @title
+#' @usage
+#' @useDynLib
+#' 
+
+CalculateC <- function(A,B){
+
+  C <- A + B
+  
+  return(C)
+  
+}
